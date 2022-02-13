d9ed0c4376c72725794637a4c807e65265050992
Mortis66666 <mortisloverbs@gmail.com>
2022-02-13 21:53:47 +0800

    Added template

---
+++ template.py
@@ -0,0 +1,45 @@
+import pygame
+
+pygame.init()
+
+
+width, height = 600, 600
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pygame title")
+
+
+# Colours
+white = (233, 233, 233)
+black = (0, 0, 0)
+
+
+class Game:
+
+    def draw(self): # the draw function
+        win.fill(white)
+
+        pygame.display.update()
+
+
+    def start(self):
+
+        # In game data
+        run = True
+
+        clock = pygame.time.Clock()
+        fps = 60 # Frame per second
+
+        while run:
+            clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+
+            self.draw()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
